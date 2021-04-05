991fac406a46d6d233dc3e23347578d435944149
rayannott <imjobjob@gmail.com>
2021-04-05 18:26:12 +0300

    is_(move and shot)_possible are a bit reworked Started working on is_ended

--- game.py
+++ game.py
@@ -29,9 +29,19 @@ class Game:
             self.field[pos.x][pos.y] = Cell(1, ind)
 
     def is_ended(self):
-        return self.is_ended_local
+        some_player_cannot_move = False
+        num_of_players = len(self.players)
+        for pl_ind in range(num_of_players):
+            if len(self.possible_moves(pl_ind)) == 0:
+                print('cannot move')
+                some_player_cannot_move = True
+                break
+
+        return self.is_ended_local or some_player_cannot_move
 
     def is_shot_possible(self, current_player_index, s):
+        if not (0 <= s.x < self.board_size.x and 0 <= s.y < self.board_size.y):
+            return False
         player = self.players[current_player_index]
         pos = player.get_trace()[-1]
         # m = the_move.get_move()
@@ -42,6 +52,8 @@ class Game:
         return is_knight(pos, s) and (player_shot or shoot_to_empty or shoot_blank)
 
     def is_move_possible(self, current_player_index, m):
+        if not (0 <= m.x < self.board_size.x and 0 <= m.y < self.board_size.y):
+            return False
         player = self.players[current_player_index]
         pos = player.get_trace()[-1]
         # m = the_move.get_move()
@@ -58,18 +70,22 @@ class Game:
             self.players[current_player_index].next_position(m)
             self.field[m.x][m.y] = Cell(1, current_player_index)
             self.field[pos.x][pos.y] = Cell(2, current_player_index)
+
+            # print possible shots after successful move
+            # print('shots:')
+            # for i in self.possible_shots(current_player_index):
+            #     print(i)
         else:
             raise ValueError('Invalid move')
 
     def execute_shot(self, current_player_index, s):
-        player = self.players[current_player_index]
         # s = the_move.get_shoot()
         if self.is_shot_possible(current_player_index, s) and \
                 current_player_index == self.current_move % self.number_of_players:
             self.current_move += 1
+            player = self.players[current_player_index]
             shoot_blank = s.x + s.y == -2 and player.get_blanks() > 0
             player_shot = self.field[s.x][s.y].is_player()
-            print(self.field[s.x][s.y])
             # TODO: blanks
             if shoot_blank:
                 player.reduce_blanks()
@@ -78,11 +94,16 @@ class Game:
             elif player_shot:
                 print('player was shot')
                 player_to_be_killed = self.field[s.x][s.y].get_owner_id()
-                self.kill_this_player(player_to_be_killed)
+                # this will be needed in version 2 (mult players)
+                # self.kill_this_player(player_to_be_killed)
                 self.field[s.x][s.y] = Cell(3, current_player_index)
                 self.is_ended_local = True  # tmp
             else:
                 self.field[s.x][s.y] = Cell(3, current_player_index)
+                # print possible moves after successful move
+                # print('moves:')
+                # for i in self.possible_moves(current_player_index):
+                #     print(i)
 
         else:
             # pos_wrong = player.get_trace()[-1]
@@ -101,11 +122,16 @@ class Game:
     def get_field(self):
         return self.field
 
-    def possible_moves(self, pos, is_knight_=False):
-        result = [pos + x for x in self.deltas[:8]]
+    def possible_moves(self, player_index):
+        player = self.players[player_index]
+        pos = player.get_trace()[-1]
+        is_knight_ = player.get_is_knight()
+        result = [pos + de for de in self.deltas[:8] if self.is_move_possible(player_index, pos+de)]
         if is_knight_:
-            result += [pos + x for x in deltas[8:]]
+            result += [pos + de for de in deltas[8:] if self.is_move_possible(player_index, pos+de)]
         return result
 
-    def possible_shots(self, pos):
-        return [pos + x for x in self.deltas[8:]]
+    def possible_shots(self, player_index):
+        player = self.players[player_index]
+        pos = player.get_trace()[-1]
+        return [pos + de for de in self.deltas[8:] if self.is_shot_possible(player_index, pos+de)]
