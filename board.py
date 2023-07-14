3f92ed5a405c39b6e2ca65996bdfba2b221aa304
Airat <imjobjob@gmail.com>
2023-07-14 15:19:38 +0200

    reorganized everything so that it looks like in a chess engine

--- board.py
+++ board.py
@@ -1,95 +1,126 @@
-from dataclasses import dataclass
-
 from cell import Cell, CellType
 import exceptions as exc
 from utils import *
 
-BOARD_SIZE = 12
-NUM_OF_PLAYERS = 2
-
-
-@dataclass
-class Pos:
-    x: int
-    y: int
-    def __add__(self, other: 'Pos') -> 'Pos':
-        return Pos(self.x + other.x, self.y + other.y)
-    
-    def __sub__(self, other: 'Pos') -> 'Pos':
-        return Pos(self.x - other.x, self.y - other.y)
-
-    def tup(self):
-        return (self.x, self.y)
-
-    def __str__(self) -> str:
-        return f'{XAXIS[self.y]}{YAXIS[self.x]}'
-
-
-MOVE_DELTAS = [Pos(0, 1), Pos(1, 0), Pos(0, -1), Pos(-1, 0),
-            Pos(1, 1), Pos(1, -1), Pos(-1, -1), Pos(-1, 1)]
-SHOT_DELTAS = [Pos(-2, -1), Pos(-1, -2), Pos(2, 1), Pos(1, 2),
-            Pos(-2, 1), Pos(2, -1), Pos(-1, 2), Pos(1, -2)]
-SHOT_DELTAS_BOUNDARY = [Pos(-2, -2), Pos(-2, 2), Pos(2, -2), Pos(2, 2)]
-
-
-@dataclass
-class CompleteMove:
-    move: Pos
-    shot: Pos
-    def __repr__(self) -> str:
-        return f'{self.move}/{self.shot}'
-
 
 class Board:
     def __init__(self) -> None:
+        self._reset()
+    
+    def info(self):
+        print(f'moves played: {len(self.move_history)}')
+        print(f'player #{self.player_to_move} moves: {self.eval_num_of_moves()}')
+        print(f'dscr: {self.get_ngd()}')
+        someone_can_win = self.can_win_by()
+        if someone_can_win and self.victor is None:
+            print(f'can win by {someone_can_win}')
+
+    def _reset(self):
         self.board = [[Cell(cell_type=CellType.EMPTY) for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
         self.players_positions = [Pos(3, 4), Pos(8, 7)]
         for pl_id, pl_pos in enumerate(self.players_positions):
             self[pl_pos.tup()].set_player_id(pl_id).set_type(CellType.PLAYER)
+        self.player_to_move = 0
+        self.victor = None
+        self.move_history: list[CompleteMove] = []
+
+    def set_state_from_ngd(self, ngd: str) -> int:
+        '''Plays the game given by the normal game description (NGD) and
+        changes the board accordingly.
+        If successful, returns the number of played moves.'''
+        self._reset()
+        for ending in ['b.', 'r.']:
+            if ngd.endswith(ending): ngd.replace(ending, '')
+        moves_normal = ngd.replace(',', '').strip().split()
+        for move_normal in moves_normal:
+            self.execute_complete_move(make(move_normal))
+        return len(moves_normal)
+
+    def get_ngd(self) -> str:
+        '''Returns the normal game description (NGD) as a string'''
+        descr = ''
+        for i, cm in enumerate(self.move_history):
+            descr += XAXIS[cm.move.y]
+            descr += YAXIS[cm.move.x]
+            descr += '/'
+            descr += XAXIS[cm.shot.y]
+            descr += YAXIS[cm.shot.x]
+            descr += ',' if i % 2 else ''
+            descr += ' '
+        if self.victor is not None: descr += ['b', 'r'][self.victor] + '.'
+        return descr
+    
+    def set_state_from_nbs(self, nbs: str) -> None:
+        pass
+
+    def get_nbs(self) -> str:
+        '''Returns the normal board state (NBS) string.
+        The NBS is equivalent to chess' FEN'''
+        #! make as in chess: using numbers for consecutive blank tiles
+        to_ret = []
+        for row_id in range(BOARD_SIZE):
+            row = ''
+            for col_id in range(BOARD_SIZE):
+                this_cell = self.board[row_id][col_id]
+                if this_cell.is_empty(): row += '.'
+                elif this_cell.is_shot(): row += 'x'
+                elif this_cell.is_trace(): row += 'T' if this_cell.player_id == 0 else 't'
+                elif this_cell.is_player(): row += 'P' if this_cell.player_id == 0 else 'p'
+            to_ret.append(row)
+        return '/'.join(to_ret)
     
     def __getitem__(self, s: tuple[int, int]) -> Cell:
         return self.board[s[0]][s[1]]
 
-    def _get_moves(self, pos: Pos) -> list[Pos]:
-        '''Returns moves possible from a given position'''
+    def get_cpp(self) -> Pos:
+        '''Returns the current player's position'''
+        return self.players_positions[self.player_to_move]
+
+    def check_victory(self) -> int | None:
+        '''Return an index of a winner or None if game is not terminated'''
+        if self[self.get_cpp().tup()].is_shot() \
+            or not self.get_complete_moves():
+            return 1 - self.player_to_move
+        return None
+
+    def _get_moves_from_for(self, move_from: Pos, move_for: int) -> list[Pos]:
+        '''Returns moves possible for the current player'''
         #? add jumping over root?
         moves = []
         for delta in MOVE_DELTAS:
-            if not 0 <= pos.x + delta.x < BOARD_SIZE or not 0 <= pos.y + delta.y < BOARD_SIZE:
+            if not 0 <= move_from.x + delta.x < BOARD_SIZE or not 0 <= move_from.y + delta.y < BOARD_SIZE:
                 continue
-            if self[pos.x + delta.x, pos.y + delta.y].is_empty():
-                moves.append(Pos(pos.x + delta.x, pos.y + delta.y))
+            if self[move_from.x + delta.x, move_from.y + delta.y].is_empty():
+                moves.append(Pos(move_from.x + delta.x, move_from.y + delta.y))
         return moves
 
-    def _get_shots(self, pos: Pos, player_id: int) -> list[Pos]:
-        '''Returns shots possible from a given position'''
+    def _get_shots_from_for(self, shot_from: Pos, shot_for: int):
         shots = []
-        for delta in SHOT_DELTAS + (SHOT_DELTAS_BOUNDARY if pos.x in {2, 9} or pos.y in {2, 9} else []):
-            if not 0 <= pos.x + delta.x < BOARD_SIZE or not 0 <= pos.y + delta.y < BOARD_SIZE:
+        for delta in SHOT_DELTAS + (SHOT_DELTAS_BOUNDARY if shot_from.x in {2, 9} or shot_from.y in {2, 9} else []):
+            if not 0 <= shot_from.x + delta.x < BOARD_SIZE or not 0 <= shot_from.y + delta.y < BOARD_SIZE:
                 continue
-            if self[pos.x + delta.x, pos.y + delta.y].is_empty() or \
-                self[pos.x + delta.x, pos.y + delta.y].is_player(1-player_id):
-                shots.append(Pos(pos.x + delta.x, pos.y + delta.y))
+            if self[shot_from.x + delta.x, shot_from.y + delta.y].is_empty() or \
+                self[shot_from.x + delta.x, shot_from.y + delta.y].is_player(1-shot_for):
+                shots.append(Pos(shot_from.x + delta.x, shot_from.y + delta.y))
         return shots
-    
-    def _if_complete_move_possible(self, player_id: int, complete_move: CompleteMove) -> bool:
-        if complete_move.move not in self._get_moves(self.players_positions[player_id]):
+        
+    def _if_complete_move_possible(self, complete_move: CompleteMove) -> bool:
+        if complete_move.move not in self._get_moves_from_for(self.get_cpp(), self.player_to_move):
             return False
-        if complete_move.shot not in self._get_shots(complete_move.move, player_id):
+        if complete_move.shot not in self._get_shots_from_for(complete_move.move, self.player_to_move):
             return False
         return True
     
-    def get_complete_moves(self, player_id: int) -> list[CompleteMove]:
-        '''Returns a list of complete moves possible from a given position'''
-        pos = self.players_positions[player_id]
+    def get_complete_moves(self) -> list[CompleteMove]:
+        '''Returns a list of complete moves possible for the current player'''
         complete_moves = []
-        for move in self._get_moves(pos):
-            for shot in self._get_shots(move, player_id):
+        for move in self._get_moves_from_for(self.get_cpp(), self.player_to_move):
+            for shot in self._get_shots_from_for(move, self.player_to_move):
                 complete_moves.append(CompleteMove(move, shot))
         return complete_moves
 
     def move_player_to(self, player_id: int, move_to: Pos):
-        if move_to in self._get_moves(self.players_positions[player_id]):
+        if move_to in self._get_moves_from_for(self.get_cpp(), self.player_to_move):
             self[self.players_positions[player_id].tup()].set_type(CellType.TRACE) # reset prev cell
             self.players_positions[player_id] = move_to # change player's pos
             self[move_to.tup()].set_player_id(player_id).set_type(CellType.PLAYER) # set new pos on the board
@@ -97,25 +128,28 @@ class Board:
             raise exc.InvalidMoveException(f'Cannot move player #{player_id} to {move_to}')
 
     def shoot_by_from_to(self, player_id: int, shoot_from: Pos, shoot_to: Pos):
-        if shoot_to in self._get_shots(shoot_from, player_id):
+        if shoot_to in self._get_shots_from_for(shoot_from, player_id):
             self[shoot_to.tup()].set_player_id(player_id).set_type(CellType.SHOT)
         else:
             raise exc.InvalidShotException(f'Cannot shoot by player #{player_id} from {shoot_from} to {shoot_to}')
 
-    def execute_complete_move_by(self, player_id: int, complete_move: CompleteMove):
-        if not self._if_complete_move_possible(player_id, complete_move):
+    def execute_complete_move(self, complete_move: CompleteMove):
+        if not self._if_complete_move_possible(complete_move):
             raise exc.InvalidCompleteMoveException('Cannot execute this move')
-        self.move_player_to(player_id, complete_move.move)
-        self.shoot_by_from_to(player_id, complete_move.move, complete_move.shot)
+        self.move_player_to(self.player_to_move, complete_move.move)
+        self.shoot_by_from_to(self.player_to_move, complete_move.move, complete_move.shot)
+        self.move_history.append(complete_move)
+        self.player_to_move = 1 - self.player_to_move
+        self.victor = self.check_victory()
     
-    def eval_num_of_moves(self, player_to_move: int) -> int:
-        return len(self.get_complete_moves(player_to_move))
+    def eval_num_of_moves(self) -> int:
+        return len(self.get_complete_moves())
 
-    def can_win_by(self, player_to_move: int) -> CompleteMove | None:
-        for cm in self.get_complete_moves(player_to_move):
-            if cm.shot == self.players_positions[1 - player_to_move]:
+    def can_win_by(self) -> CompleteMove | None:
+        for cm in self.get_complete_moves():
+            if cm.shot == self.players_positions[1 - self.player_to_move]:
                 return cm
         return None
 
     def eval_num_of_non_lethal_moves(self, player_to_move: int) -> int:
-        pass
+        return 0
--- utils.py
+++ utils.py
@@ -1,4 +1,54 @@
+from dataclasses import dataclass
+
 YAXIS = 'ba9876543210'
 XAXIS = 'abcdefghijkl'
 YAXIS_BACK = {ch: i for i, ch in enumerate(YAXIS)}
 XAXIS_BACK = {ch: i for i, ch in enumerate(XAXIS)}
+
+
+BOARD_SIZE = 12
+NUM_OF_PLAYERS = 2
+
+
+def make_move(str_coord: str) -> 'Pos':
+    return Pos(YAXIS_BACK[str_coord[1]], XAXIS_BACK[str_coord[0]])
+
+def make_cm(str_coord_move: str, str_coord_shot: str) -> 'CompleteMove':
+    return CompleteMove(make_move(str_coord_move), make_move(str_coord_shot))
+
+def make(cm_normal: str) -> 'CompleteMove':
+    '''Returns a CompleteMove object constructed with
+    the complete move normal notation'''
+    return make_cm(*cm_normal.split('/'))
+
+
+@dataclass
+class Pos:
+    x: int
+    y: int
+    def __add__(self, other: 'Pos') -> 'Pos':
+        return Pos(self.x + other.x, self.y + other.y)
+    
+    def __sub__(self, other: 'Pos') -> 'Pos':
+        return Pos(self.x - other.x, self.y - other.y)
+
+    def tup(self):
+        return (self.x, self.y)
+
+    def __str__(self) -> str:
+        return f'{XAXIS[self.y]}{YAXIS[self.x]}'
+
+
+MOVE_DELTAS = [Pos(0, 1), Pos(1, 0), Pos(0, -1), Pos(-1, 0),
+            Pos(1, 1), Pos(1, -1), Pos(-1, -1), Pos(-1, 1)]
+SHOT_DELTAS = [Pos(-2, -1), Pos(-1, -2), Pos(2, 1), Pos(1, 2),
+            Pos(-2, 1), Pos(2, -1), Pos(-1, 2), Pos(1, -2)]
+SHOT_DELTAS_BOUNDARY = [Pos(-2, -2), Pos(-2, 2), Pos(2, -2), Pos(2, 2)]
+
+
+@dataclass
+class CompleteMove:
+    move: Pos
+    shot: Pos
+    def __repr__(self) -> str:
+        return f'{self.move}/{self.shot}'
