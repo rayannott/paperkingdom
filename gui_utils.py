7da466a968eef4970eb703db292b850adc9b7491
Airat <imjobjob@gmail.com>
2023-07-19 18:43:59 +0200

    adjusted modules

--- gui_utils.py
+++ gui_utils.py
@@ -12,33 +12,30 @@ def init_layout(width, height, l_margin, r_margin, button_margin):
 
     manager = pygame_gui.UIManager((width, height), 'theme.json')
     clock = pygame.time.Clock()
-    base_panel = pygame_gui.elements.ui_panel.UIPanel(
+    base_panel = pygame_gui.elements.UIPanel(
         relative_rect=pygame.Rect((0, 0), (width, height)),
         manager=manager,
-        starting_layer_height=1,
         object_id='base'
     )
     board_size = min(width - l_margin - r_margin, height)
     left_margin = (width - board_size) // 2
     top_margin = (height - board_size) // 2
-    board = pygame_gui.elements.ui_panel.UIPanel(
+    board = pygame_gui.elements.UIPanel(
         relative_rect=pygame.Rect((left_margin, top_margin), (board_size, board_size)),
         manager=manager,
-        starting_layer_height=1,
         container=base_panel,
         object_id='board'
     )
-    left_board = pygame_gui.elements.ui_panel.UIPanel(
+    left_board = pygame_gui.elements.UIPanel(
         relative_rect=pygame.Rect((0, 0), (left_margin, height)),
         manager=manager,
-        starting_layer_height=1,
         container=base_panel,
         object_id='leftboard'
     )
     total_empty = button_margin * 3
     button_size_x = (left_margin - total_empty) / 2
     button_size_y = 20
-    swap_button = pygame_gui.elements.ui_button.UIButton(
+    swap_button = pygame_gui.elements.UIButton(
         relative_rect=pygame.Rect(button_margin, button_margin, button_size_x, button_size_y),
         text='Swap',
         manager=manager,
@@ -51,7 +48,7 @@ def init_layout(width, height, l_margin, r_margin, button_margin):
             'bottom': 'bottom'
         }
     )
-    tetra_button = pygame_gui.elements.ui_button.UIButton(
+    tetra_button = pygame_gui.elements.UIButton(
         relative_rect=pygame.Rect(left_margin - button_margin - button_size_x, button_margin, button_size_x, button_size_y),
         text='Tetra',
         manager=manager,
@@ -64,7 +61,7 @@ def init_layout(width, height, l_margin, r_margin, button_margin):
             'bottom': 'bottom'
         }
     )
-    restart_button = pygame_gui.elements.ui_button.UIButton(
+    restart_button = pygame_gui.elements.UIButton(
         relative_rect=pygame.Rect(button_margin, 2 * button_margin + button_size_y, button_size_x, button_size_y),
         text='Restart',
         manager=manager,
@@ -92,7 +89,7 @@ def generate_field(manager, board, game, current_player):
         for indcell, cell in enumerate(row):
             rect = pygame.Rect(w * indrow, h * indcell, w, h)
             if cell.is_player():
-                butt = pygame_gui.elements.ui_button.UIButton(
+                butt = pygame_gui.elements.UIButton(
                     relative_rect=rect,
                     text=str(cell),
                     manager=manager,
@@ -102,7 +99,7 @@ def generate_field(manager, board, game, current_player):
                 if current_player == cell.get_owner_id():
                     butt.disable()
             else:
-                butt = pygame_gui.elements.ui_button.UIButton(
+                butt = pygame_gui.elements.UIButton(
                     relative_rect=rect,
                     text=str(cell),
                     manager=manager,
@@ -115,7 +112,7 @@ def generate_field(manager, board, game, current_player):
 def kill_create_button(buttons, index1, index2, text, manager, board, object_id, disable):
     rect = buttons[index1][index2].get_relative_rect()
     buttons[index1][index2].kill()
-    buttons[index1][index2] = pygame_gui.elements.ui_button.UIButton(
+    buttons[index1][index2] = pygame_gui.elements.UIButton(
         relative_rect=rect,
         text=text,
         manager=manager,
